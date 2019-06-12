a7d21ecd51df345f952ba09b17b7089038cf21cd
Derk Gortemaker <derk@gortemaker.nl>
2019-06-12 15:46:59 +0200

    add index_version option

--- manifests/server/config.pp
+++ manifests/server/config.pp
@@ -38,6 +38,7 @@ class influxdb::server::config {
   $compact_full_write_cold_duration             = $influxdb::server::compact_full_write_cold_duration
   $max_points_per_block                         = $influxdb::server::max_points_per_block
   $max_series_per_database                      = $influxdb::server::max_series_per_database
+  $index_version                                = $influxdb::server::index_version
 
   $hinted_handoff_enabled                       = $influxdb::server::hinted_handoff_enabled
   $hinted_handoff_dir                           = $influxdb::server::hinted_handoff_dir
